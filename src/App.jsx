a13662e1699468495d5b6559a8040cd7036c0e6d
Keith <kkwilson27@hotmail.com>
2023-09-27 14:34:09 -0400

    connected to mongo

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,11 @@ import SignUp from './pages/SignUp';
 import Signin from './pages/Signin';
 import About from './pages/About';
 import Profile from './pages/Profile';
+import Header from './components/Header';
 
 export default function App() {
-  return <BrowserRouter>
+  return <BrowserRouter>  
+    <Header/>
       <Routes>
         <Route path='/' element={<Home/>} />
         <Route path='/sign-in' element={<Signin/>} />
